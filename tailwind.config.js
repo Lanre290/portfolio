d7e10ff18dc08eb79aa8d0bd941ae35d1bb69ff1
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-07-30 15:54:36 +0100

    chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}'
+  ],
+  // ...
 }
-
