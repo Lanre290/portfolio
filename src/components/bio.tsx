fe8ca24f531173b9dbe6b5ad510dde2adf3b8384
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-07-31 00:55:06 +0100

    minor fixes

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -2,7 +2,7 @@ import { Fade } from "react-awesome-reveal";
 
 function Bio(){
     return(
-        <Fade direction='up' duration={700} className="m-5 w-5/6 md:w-2/5 sm:w-5/6 shadow-lg mt-20  p-10 rounded-xl" cascade>
+        <Fade direction='right' duration={700} className="m-5 w-5/6 md:w-2/5 sm:w-5/6 shadow-lg mt-20  p-10 rounded-xl" cascade>
             <div>
                 <h3 className="under-line px-5 w-fit py-4 text-3xl" style = {{width: 'fit-content'}}>Bio</h3>
                 <h3 className="mt-10">
