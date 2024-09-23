d73b35050afbad117021a38e7e17a75df0e562be
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-09-23 23:20:11 +0100

    config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}'
+    './pages/**/*.{html,js, tsx}',
+    './components/**/*.{html,js, tsx}',
   ],
   // ...
 }
